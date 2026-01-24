bcc65f6cad8fb251c5f0702dc7dbffcb1bf8e738
macbook <macbook@macbooks-MacBook-Pro-2.local>
2026-01-24 14:07:25 +0700

    DOM SELESAI

--- belajarJS/belajarDOM.js
+++ belajarJS/belajarDOM.js
@@ -42,9 +42,38 @@ klik1.style.padding = "10px 20px"
 klik1.style.fontSize = "20px"
 klik1.style.backgroundColor = "blue"
 klik1.style.color = "white"
+
+//akan muncul alert ketika di klik
+klik1.addEventListener("click", () => {
+    alert("jangan di pencet woi")
+})
+//akan merubah warna hijau ketika kursor meninggalkan tombol
+klik1.addEventListener("mouseleave", () => {
+    klik1.style.backgroundColor = "blue"
+})
+//akan merubah warna hitam ketika kursor di arahkan ke tombol
+klik1.addEventListener("mouseover", () => {
+    klik1.style.backgroundColor = "black"
+})
+
+//NOTE: PADA JS TIDAK MENGGUNAKAN KATA "ON" PADA EVENTLISTENER
+//MISAL JIKA DI HTML ADA ONCLICK MAKA DI JS HANYA CLICK SAJA
+
+
 const klik2 = document.getElementById("btn2")
 klik2.style.padding = "10px 20px"
 klik2.style.fontSize = "20px"
 klik2.style.backgroundColor = "red"
+
+klik2.addEventListener("mouseover", () => {
+    klik2.style.backgroundColor = "yellow"
+    const paragrafbaru= document.createElement("p")
+    paragrafbaru.textContent = "tolong jangan arahkan kursor ke tombol"
+    body.append(paragrafbaru)
+})
+
+klik2.addEventListener("mouseleave", () => {
+    klik2.style.backgroundColor = "red"
+})
 
 console.log(klik1)

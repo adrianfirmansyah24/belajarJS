241e7a4b4395f34cde39b7ac3e73351112ee234b
macbook <macbook@macbooks-MacBook-Pro-2.local>
2026-01-25 19:30:57 +0700

    ppp

--- belajarJS/belajarArray.js
+++ belajarJS/belajarArray.js
@@ -40,3 +40,51 @@ if(kucing) {
 } else {
     console.log("tidak ada kucing di array hewan")
 }
+
+//bisa melihat juga data sebelum dan sesudah dalam array
+const kangkung = sayur.includes('kangkung')
+
+if (kangkung) {
+    const posisikangkung = sayur.indexOf('kangkung')
+    const sebelumkangkung = posisikangkung - 1
+    const sesudahkangkung = posisikangkung + 1
+    const before = sayur[sebelumkangkung]
+    const after = sayur[sesudahkangkung]
+    console.log(posisikangkung)
+    console.log(sebelumkangkung)
+    console.log(sesudahkangkung)
+    console.log(before)
+    console.log(after)
+}
+
+//shift() dan pop() = menghapus data dalam array depan dan belakang
+//ex shift() 
+//const buahdepan = buah.shft()
+//ex pop()
+//const buahbelakang = buah.pop()
+
+
+//melihat data pertama pada array
+const buahdepan = buah[0]
+console.log(buahdepan)
+
+//melhat data akhir pada array
+const buahbelakang = buah[buah.length-1]
+console.log(buahbelakang)
+
+const panjangbuah = buah.length
+console.log(panjangbuah)
+
+//menambah data pada array menggunakan push() dan unshift()
+const nambahbuah1 = buah.push('semangka')//menambah data di belakang array
+console.log(buah)
+
+const nambahbuah2 = buah.unshift('kiwi')//menambah data di depan array
+console.log(buah)
+ 
+
+const umur = []
+umur['dua belas'] = 12
+umur['tiga belas'] = 13
+umur['empat belas'] = 14
+console.log(umur)
